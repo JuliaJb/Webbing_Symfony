e6ffbb461b682f57a9ae5055ea85f5489425b56c
Julia Jacob <Julia@MacBook-Air-de-Julia.local>
2016-05-18 00:08:25 +0200

    les routes

--- src/JJ/WebbingBundle/Controller/DefaultController.php
+++ src/JJ/WebbingBundle/Controller/DefaultController.php
@@ -6,8 +6,18 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    public function indexAction()
+    public function homeAction()
     {
-        return $this->render('JJWebbingBundle:Default:index.html.twig');
+        return $this->render('JJWebbingBundle:Default:home.html.twig');
+    }
+
+    public function loginAction()
+    {
+        return $this->render('JJWebbingBundle:Default:login.html.twig');
+    }
+
+    public function profilAction()
+    {
+        return $this->render('JJWebbingBundle:Default:profil.html.twig');
     }
 }
